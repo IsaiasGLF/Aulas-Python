7b70dec4f452a92c9c89ef07c3104757842f2515
IsaiasGLF <160743931+IsaiasGLF@users.noreply.github.com>
2024-05-03 12:23:11 +0000

    Aula - 1

---
+++ 1.tipos de dadis/2 tipagem dinamica.py
@@ -0,0 +1,10 @@
+# Atribuindo valores
+nome = "Marta"
+idade = 20
+peso = 62.500
+
+# Exibindo resultados
+# simples
+print ("\nnome:", nome)
+print ("\nidade:", idade)
+print ("\npeso:", peso)
---
+++ 1.tipos de dadis/3 tipagem estática.py
@@ -0,0 +1,10 @@
+# Atribuindo valores
+nome: str = "Marta"
+idade: int = 20
+peso: float = 62.500
+
+# Exibindo resultados
+# estática
+print (f"nome : {nome}")
+print (f"idade : {idade}")
+print (f"peso : {peso}")
